36986c60b9178550d022fa3b555a084a075c798c
guadalupecuascota <guadalupecuascota@localhost>
2019-07-09 09:27:38 -0500

    Cambios Rol Cajero

---
+++ P_AhorroyCredito_PF_V1EJB/ejbModule/proyecto/model/manager/ManagerCajeroTransaccion.java
@@ -0,0 +1,101 @@
+package proyecto.model.manager;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import proyecto.model.entities.CuentaCliente;
+import proyecto.model.entities.Transaccion;
+import proyecto.model.entities.Usuario;
+import proyecto.model.entities.TipoTransaccion;
+
+
+
+/**
+ * Session Bean implementation class ManagerCajeroTransaccion
+ */
+@Stateless
+@LocalBean
+public class ManagerCajeroTransaccion {
+	 @PersistenceContext
+	 private EntityManager em;
+    /**
+     * Default constructor. 
+     */
+    public ManagerCajeroTransaccion() {
+       
+    }
+    public List<CuentaCliente> findAllcuentaCliente() {
+		String consulta = "SELECT c FROM CuentaCliente c";
+		Query q = em.createQuery(consulta, CuentaCliente.class);
+		return q.getResultList();
+	}
+    public CuentaCliente buscarCuentaCliente(int nroCuenta) {
+    	return em.find(CuentaCliente.class,nroCuenta);
+    }
+    
+    public List<Usuario> findAllUsuarios() {
+  		String consulta= "SELECT u FROM Usuario u";
+  		Query q = em.createQuery(consulta,Usuario.class);
+  		return q.getResultList();
+  	}
+    public Usuario buscarUsuario(int idUsuario) {
+    	return em.find(Usuario.class,idUsuario);
+    }
+    public List<Transaccion> findAllTransaccion() {
+  		String consulta= "SELECT t FROM Transaccion t";
+  		Query q = em.createQuery(consulta,Transaccion.class);
+  		return q.getResultList();
+  	}
+    public Transaccion buscarTransaccion(int idTransaccion) {
+    	return em.find(Transaccion.class,idTransaccion);
+    }
+    public List<TipoTransaccion> findAllTipoTransaccion() {
+  		String consulta= "SELECT t FROM TipoTransaccion t";
+  		Query q = em.createQuery(consulta,TipoTransaccion.class);
+  		return q.getResultList();
+  	}
+    public TipoTransaccion buscarTipoTransaccion(int idTipoTransaccion) {
+    	return em.find(TipoTransaccion.class,idTipoTransaccion);
+    }
+    public void insertarTransaccion(int nroCuenta, int idTipoTransaccion , BigDecimal montoTransaccion ,Date fechaTransaccion,Integer saldoTransaccion)  {
+    	Transaccion transaccion=new Transaccion();
+     	CuentaCliente cuentaCliente=buscarCuentaCliente(nroCuenta);
+     	
+        TipoTransaccion tipoTransaccion=buscarTipoTransaccion(idTipoTransaccion);
+     	
+     	transaccion.setCuentaCliente(cuentaCliente);
+     	transaccion.setTipoTransaccion(tipoTransaccion);
+     	transaccion.setMontoTransaccion(montoTransaccion);
+     	transaccion.setFechaTransaccion(fechaTransaccion); 
+     	transaccion.setSaldoTransaccion(saldoTransaccion);
+     	em.persist(transaccion);
+     
+    }
+    public void eliminarTransaccion(int idTransaccion ) {
+    	Transaccion transaccion =buscarTransaccion(idTransaccion);
+    	if(transaccion!=null)
+    		em.remove(transaccion);
+    }
+   
+    public void actuaizarTransferencia(Transaccion transaccion)  throws Exception{
+    	Transaccion e=buscarTransaccion(transaccion.getIdTransaccion());
+    	if(e==null)
+    		throw new Exception("No existe la transaccion.");
+    	e.setCuentaCliente(transaccion.getCuentaCliente());
+    	e.setTipoTransaccion(transaccion.getTipoTransaccion());
+    	e.setMontoTransaccion(transaccion.getMontoTransaccion());
+    	e.setSaldoTransaccion(transaccion.getSaldoTransaccion());
+    	em.merge(e);
+    }
+  
+    
+}
+	
---
+++ P_AhorroyCredito_PF_V1Web/src/proyecto/controller/model/BeanCajeroTransaccion.java
@@ -0,0 +1,211 @@
+package proyecto.controller.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import proyecto.model.entities.CuentaCliente;
+import proyecto.model.entities.TipoTransaccion;
+import proyecto.model.entities.Transaccion;
+import proyecto.model.entities.Usuario;
+import proyecto.model.manager.ManagerCajeroTransaccion;
+import proyecto.controller.model.JSFUtil;
+
+
+@Named
+@SessionScoped
+public class BeanCajeroTransaccion implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private ManagerCajeroTransaccion managerCajeroTransaccion;
+	private List<CuentaCliente> listaCuentaCliente;
+	private List<Usuario> listaUsuario;
+	private List<Transaccion> listaTransaccion;
+	private List<TipoTransaccion> listaTipoTransaccion;
+
+	private int nroCuenta;
+	private int idTipoTransaccion;
+	private BigDecimal montoTransaccion;
+	private Date fechaTransaccion;
+	private Integer saldoTransaccion;
+    private Transaccion transaccion;
+    
+    private TipoTransaccion tipoTransaccion;
+    private CuentaCliente cuentaCliente;
+    private boolean panelColapsado;
+    private Transaccion transaccionSeleccionado;
+    
+    SimpleDateFormat Date = new SimpleDateFormat("dd/MM/yyyy");
+	@PostConstruct
+	public void inicializar() {
+		listaCuentaCliente = managerCajeroTransaccion.findAllcuentaCliente();
+		listaUsuario = managerCajeroTransaccion.findAllUsuarios();
+		listaTransaccion = managerCajeroTransaccion.findAllTransaccion();
+		listaTipoTransaccion = managerCajeroTransaccion.findAllTipoTransaccion();
+		transaccion = new Transaccion();
+		tipoTransaccion=new TipoTransaccion();
+		cuentaCliente=new CuentaCliente();
+		panelColapsado=true;
+		
+
+	}
+	
+	public void actionListenerColapsarPanel() {
+		panelColapsado=!panelColapsado;
+	}
+	
+
+	public void actionListenerInsertarTransaccion() {
+		try {
+
+			managerCajeroTransaccion.insertarTransaccion(nroCuenta, idTipoTransaccion,montoTransaccion,fechaTransaccion,saldoTransaccion);
+			listaTransaccion = managerCajeroTransaccion.findAllTransaccion();
+			transaccion = new Transaccion();
+			JSFUtil.crearMensajeInfo("Transacción realizada");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	public void actionListenerEliminarTranasaccion(int idTransaccion) {
+		managerCajeroTransaccion.eliminarTransaccion(idTransaccion);
+		listaTransaccion=managerCajeroTransaccion.findAllTransaccion();
+				JSFUtil.crearMensajeInfo("Transacción Eliminado");
+	}
+	public void actionListenerSeleccionarTransaccion(Transaccion transaccion) {
+		transaccionSeleccionado=transaccion;
+	}
+	public void actionListenerActualizarTransaccion() {
+	try {
+		managerCajeroTransaccion.actuaizarTransferencia(transaccionSeleccionado);
+		listaTransaccion=managerCajeroTransaccion.findAllTransaccion();
+		JSFUtil.crearMensajeInfo("Datos Actualizados");
+		
+	}catch (Exception e) {
+		JSFUtil.crearMensajeError(e.getMessage());
+		e.printStackTrace();
+	}
+	}
+	
+
+	public Integer getSaldoTransaccion() {
+		return saldoTransaccion;
+	}
+
+	public void setSaldoTransaccion(Integer saldoTransaccion) {
+		this.saldoTransaccion = saldoTransaccion;
+	}
+
+	public List<CuentaCliente> getListaCuentaCliente() {
+		return listaCuentaCliente;
+	}
+
+	public void setListaCuentaCliente(List<CuentaCliente> listaCuentaCliente) {
+		this.listaCuentaCliente = listaCuentaCliente;
+	}
+
+	public List<Usuario> getListaUsuario() {
+		return listaUsuario;
+	}
+
+	public void setListaUsuario(List<Usuario> listaUsuario) {
+		this.listaUsuario = listaUsuario;
+	}
+
+	public List<Transaccion> getListaTransaccion() {
+		return listaTransaccion;
+	}
+
+	public void setListaTransaccion(List<Transaccion> listaTransaccion) {
+		this.listaTransaccion = listaTransaccion;
+	}
+
+	public List<TipoTransaccion> getListaTipoTransaccion() {
+		return listaTipoTransaccion;
+	}
+
+	public void setListaTipoTransaccion(List<TipoTransaccion> listaTipoTransaccion) {
+		this.listaTipoTransaccion = listaTipoTransaccion;
+	}
+
+	public Transaccion getTransaccion() {
+		return transaccion;
+	}
+
+	public void setTransaccion(Transaccion transaccion) {
+		this.transaccion = transaccion;
+	}
+
+	public TipoTransaccion getTipoTransaccion() {
+		return tipoTransaccion;
+	}
+
+	public void setTipoTransaccion(TipoTransaccion tipoTransaccion) {
+		this.tipoTransaccion = tipoTransaccion;
+	}
+
+	public CuentaCliente getCuentaCliente() {
+		return cuentaCliente;
+	}
+
+	public void setCuentaCliente(CuentaCliente cuentaCliente) {
+		this.cuentaCliente = cuentaCliente;
+	}
+
+	public int getNroCuenta() {
+		return nroCuenta;
+	}
+
+	public void setNroCuenta(int nroCuenta) {
+		this.nroCuenta = nroCuenta;
+	}
+
+	public int getIdTipoTransaccion() {
+		return idTipoTransaccion;
+	}
+
+	public void setIdTipoTransaccion(int idTipoTransaccion) {
+		this.idTipoTransaccion = idTipoTransaccion;
+	}
+
+	public BigDecimal getMontoTransaccion() {
+		return montoTransaccion;
+	}
+
+	public void setMontoTransaccion(BigDecimal montoTransaccion) {
+		this.montoTransaccion = montoTransaccion;
+	}
+	
+	
+	
+	public Date getFechaTransaccion() {
+		return fechaTransaccion;
+	}
+	public void setFechaTransaccion(Date fechaTransaccion) {
+		this.fechaTransaccion = fechaTransaccion;
+	}
+	public boolean isPanelColapsado() {
+		return panelColapsado;
+	}
+	public void setPanelColapsado(boolean panelColapsado) {
+		this.panelColapsado = panelColapsado;
+	}
+	public Transaccion getTransaccionSeleccionado() {
+		return transaccionSeleccionado;
+	}
+	public void setTransaccionSeleccionado(Transaccion transaccionSeleccionado) {
+		this.transaccionSeleccionado = transaccionSeleccionado;
+	}
+	
+	
+
+}

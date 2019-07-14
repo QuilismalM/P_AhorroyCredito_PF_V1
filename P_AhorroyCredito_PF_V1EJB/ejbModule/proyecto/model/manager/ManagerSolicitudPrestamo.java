3378e035b8d97da64d7dfa97e39eeb49dfbd969c
quilismalm <quilismalm@localhost>
2019-07-14 18:35:45 -0500

    estructura Solicitud de prestamo

---
+++ P_AhorroyCredito_PF_V1EJB/ejbModule/proyecto/model/manager/ManagerSolicitudPrestamo.java
@@ -0,0 +1,20 @@
+package proyecto.model.manager;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerSolicitudPrestamo
+ */
+@Stateless
+@LocalBean
+public class ManagerSolicitudPrestamo {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerSolicitudPrestamo() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ P_AhorroyCredito_PF_V1Web/src/proyecto/controller/model/BeanSolicitudPrestamo.java
@@ -0,0 +1,19 @@
+package proyecto.controller.model;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class BeanSolicitudPrestamo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}

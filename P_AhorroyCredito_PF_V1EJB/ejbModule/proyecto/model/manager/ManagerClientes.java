fc8e540e351315371ff5c9a495b5bfe93e48338f
marcelo <marcelo@localhost>
2019-07-16 11:37:29 -0500

    funcionamiento de transferecias y busquedas de movimientos

--- P_AhorroyCredito_PF_V1EJB/ejbModule/proyecto/model/manager/ManagerClientes.java
+++ P_AhorroyCredito_PF_V1EJB/ejbModule/proyecto/model/manager/ManagerClientes.java
@@ -1,5 +1,8 @@
 package proyecto.model.manager;
 
+
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.LocalBean;
@@ -9,7 +12,9 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import proyecto.model.entities.CuentaCliente;
+import proyecto.model.entities.TipoTransaccion;
 import proyecto.model.entities.Transaccion;
+
 
 /**
  * Session Bean implementation class ManagerClientes
@@ -40,11 +45,70 @@ public class ManagerClientes {
      }
      
      public List<Transaccion> SearchMovimientos(String fecchainicial, String tipomovimiento){
-    	 String consulta = "select * from Transaccion where fecha_transaccion>='"+ fecchainicial
-    	 +"' and id_tipo_transaccion="+ tipomovimiento+";";
+    	 if(!tipomovimiento.equals("General")) {
+    	 String consulta = "select t from Transaccion  t where fecha_transaccion>='"+ fecchainicial
+    	 +"' and id_tipo_transaccion=" + tipomovimiento;
     	 Query q = em.createQuery(consulta, Transaccion.class);
     	 return q.getResultList();
+    	 }else {
+    		 return findAllTransaccion();
+    	 }
      }
+     
+     public void realizarTransferencia(int cuenta_origen, int cuenta_destino, int  cantidad) {
+//    	 Date fecha_Sist = new Date();
+//    	 SimpleDateFormat formato = new SimpleDateFormat("dd/MM/YYYY");
+//    	 String fecha_sistema = formato.format(fecha_Sist);
+//    	 transaccion.setFechaTransaccion(fe);
+
+    	 Transaccion transaccion = new Transaccion();
+    	 
+    	 int saldo_actual_cnt_origen= ConsultarSaldo(cuenta_origen);
+    	 int saldo_actual_cnt_destino = ConsultarSaldo(cuenta_destino);
+    	 CuentaCliente cuenta =  findCuentaClienteByNroCuenta(cuenta_origen);
+    	 TipoTransaccion tipotransaccion = findTipoTransaccion(3);
+    	 
+    	// insercion de datos en la tabla Transaccion
+    	 transaccion.setCuentaCliente(cuenta);
+    	 transaccion.setCuentaDestino(cuenta_destino);
+    	 transaccion.setMontoTransaccion(new BigDecimal(cantidad));
+    	 transaccion.setFechaTransaccion(new Date());
+    	 transaccion.setTipoTransaccion(tipotransaccion);
+    	 transaccion.setSaldoTransaccion( new BigDecimal( saldo_actual_cnt_origen-cantidad));
+    	 em.persist(transaccion);
+    	 
+    	 //actulizamos el valor del saldo de las cuenta origen
+    	 cuenta = findCuentaClienteByNroCuenta(cuenta_origen);
+    	 cuenta.setSaldoCuenta(new BigDecimal(saldo_actual_cnt_origen-cantidad));
+    	 em.merge(cuenta);
+    	 
+    	 //actualizamos el saldo de la cuenta destino
+    	 cuenta = findCuentaClienteByNroCuenta(cuenta_destino);
+    	 cuenta.setSaldoCuenta(new BigDecimal(saldo_actual_cnt_destino+cantidad) );
+    	 em.merge(cuenta);	 
+    	 
+     }
+     
+     public int ConsultarSaldo(int nro_cuenta) {
+    	 int saldoactual=0;
+    	 String consulta = "select cl from  CuentaCliente cl where nro_cuenta_cl=" + nro_cuenta;
+    	 Query q = em.createQuery(consulta, CuentaCliente.class);
+    	 List<CuentaCliente> list = q.getResultList();
+    	 for (CuentaCliente c : list) {
+    		 saldoactual= c.getSaldoCuenta().intValue();    // convertimos getsaldocuenta de Bigdecimal a int c0n intValue()
+			 //System.out.println("impresion de consulta de saldo="+ saldoactual);
+    	 }
+    	    	 
+    	 return saldoactual;
+     }
+     
+       public CuentaCliente findCuentaClienteByNroCuenta(int nro_cuenta) {
+    	return em.find(CuentaCliente.class, nro_cuenta);
+    }
+       
+    public TipoTransaccion findTipoTransaccion(int n) {
+    	return em.find(TipoTransaccion.class, n );
+    }
 
 
 }
--- P_AhorroyCredito_PF_V1Web/src/proyecto/controller/model/BeanClientes.java
+++ P_AhorroyCredito_PF_V1Web/src/proyecto/controller/model/BeanClientes.java
@@ -9,6 +9,8 @@ import proyecto.model.entities.Transaccion;
 import proyecto.model.manager.ManagerClientes;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Named
@@ -21,11 +23,11 @@ public class BeanClientes implements Serializable {
 	private List<Transaccion> listaCli;
 	private List<Transaccion> list2;
 	private Transaccion transaccion;
-	private String fechainicial;
+	private Date fechainicial;
 	private String tipomovimiento;
-	private String cuenta_origen;
-	private String cuenta_destino;
-	private String cantidad; 
+	private int cuenta_origen;
+	private int cuenta_destino;
+	private int cantidad; 
 	
 	
 	@PostConstruct
@@ -65,7 +67,9 @@ public class BeanClientes implements Serializable {
 	}
 	
 	public void imprimirDatos() {
-		System.out.println("fecha: "+ fechainicial);
+		SimpleDateFormat fecha = new SimpleDateFormat("dd/MM/YYYY");
+		
+		System.out.println("fecha: "+ fecha.format(fechainicial));
 		System.out.println("Tipo movimiento: "+ tipomovimiento);
 		if(fechainicial.equals("01/07/2019"))
 			System.out.println("si son iguales");
@@ -73,47 +77,18 @@ public class BeanClientes implements Serializable {
 			System.out.println("no entro en el if");
 	}
 		
+	public void actionRealizarTransferecia() {
+		try {
+			managercli.realizarTransferencia(cuenta_origen, cuenta_destino, cantidad);
+			transaccion = new Transaccion();
+			JSFUtil.crearMensajeInfo("Transacción Realizada con éxito");
+		} catch (Exception e) {
+			JSFUtil.crearMensajeError(e.getMessage());
+			e.printStackTrace();
+		}
+		
+	}
 	
-	public String getFechainicial() {
-		return fechainicial;
-	}
-
-	public void setFechainicial(String fechainicial) {
-		this.fechainicial = fechainicial;
-	}
-
-	public String getTipomovimiento() {
-		return tipomovimiento;
-	}
-
-	public void setTipomovimiento(String tipomovimiento) {
-		this.tipomovimiento = tipomovimiento;
-	}
-
-	public String getCuenta_origen() {
-		return cuenta_origen;
-	}
-
-	public void setCuenta_origen(String cuenta_origen) {
-		this.cuenta_origen = cuenta_origen;
-	}
-
-	public String getCuenta_destino() {
-		return cuenta_destino;
-	}
-
-	public void setCuenta_destino(String cuenta_destino) {
-		this.cuenta_destino = cuenta_destino;
-	}
-
-	public String getCantidad() {
-		return cantidad;
-	}
-
-	public void setCantidad(String cantidad) {
-		this.cantidad = cantidad;
-	}
-
 	public String actionListenerMovimientos() {
 		return "Movimientos";
 	}
@@ -129,11 +104,61 @@ public class BeanClientes implements Serializable {
 	
 	
 	public void actionBuscarporFecha() {
-		listaCli=managercli.SearchMovimientos(fechainicial, tipomovimiento);
+		SimpleDateFormat date = new SimpleDateFormat("dd/MM/YYYY");
+		String fecha = date.format(fechainicial);
+		listaCli=managercli.SearchMovimientos(fecha, tipomovimiento);
 
 	}
 	
+////////////////////////////////////// get y set ////////////////////////////////////////////////////	
 	
+	public Date getFechainicial() {
+		return fechainicial;
+	}
+
+
+	public void setFechainicial(Date fechainicial) {
+		this.fechainicial = fechainicial;
+	}
+
+
+	public String getTipomovimiento() {
+		return tipomovimiento;
+	}
+
+	public void setTipomovimiento(String tipomovimiento) {
+		this.tipomovimiento = tipomovimiento;
+	}	
+
+	public int getCuenta_origen() {
+		return cuenta_origen;
+	}
+
+
+	public void setCuenta_origen(int cuenta_origen) {
+		this.cuenta_origen = cuenta_origen;
+	}
+
+
+	public int getCuenta_destino() {
+		return cuenta_destino;
+	}
+
+
+	public void setCuenta_destino(int cuenta_destino) {
+		this.cuenta_destino = cuenta_destino;
+	}
+
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
 
 	public Transaccion getTransaccion() {
 		return transaccion;
